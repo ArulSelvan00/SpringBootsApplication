c46d0f9d70989aa681a2a7f80b884601ca185d45
ArulSelvan00 <arulselvanofficial5@gmail.com>
2025-06-12 12:05:31 +0530

    FoodItemController.java

--- src/main/java/com/SpringCrud/SpringAngular/controller/FoodItemController.java
+++ src/main/java/com/SpringCrud/SpringAngular/controller/FoodItemController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*; // ✅ Needed for annotations 
 
 import java.util.List;
 import java.util.Optional;
+@CrossOrigin(origins = "https://strong-praline-d86c79.netlify.app/")
 
 @RestController
 @RequestMapping("/api/food_item")
