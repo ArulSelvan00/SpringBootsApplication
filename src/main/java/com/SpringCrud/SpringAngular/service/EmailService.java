115d723fb1a9fb80ba6cb92c3ac01e6d6897ac35
ArulSelvan00 <arulselvanofficial5@gmail.com>
2025-06-12 22:48:37 +0530

    Add files via upload

---
+++ src/main/java/com/SpringCrud/SpringAngular/service/EmailService.java
@@ -0,0 +1,22 @@
+package com.SpringCrud.SpringAngular.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendOrderEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("your-email@gmail.com"); // Change this to your email
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
