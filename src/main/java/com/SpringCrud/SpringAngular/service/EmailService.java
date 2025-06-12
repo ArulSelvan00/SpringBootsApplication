f7e148ad0e26ac8dc2489f3e879bf9d9840d43f9
ArulSelvan00 <arulselvanofficial5@gmail.com>
2025-06-13 00:08:38 +0530

    EmailService.java

--- src/main/java/com/SpringCrud/SpringAngular/service/EmailService.java
+++ src/main/java/com/SpringCrud/SpringAngular/service/EmailService.java
@@ -1,19 +1,22 @@
 package com.SpringCrud.SpringAngular.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendOrderEmail(String toEmail, String subject, String body) {
+    @Autowired
+    private Environment env;
+
+    public void sendOrderConfirmationEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("arulselvanofficial5@gmail.com"); // Change this to your email
+        message.setFrom(env.getProperty("spring.mail.username")); // sender
         message.setTo(toEmail);
         message.setSubject(subject);
         message.setText(body);
