ab0fbccc3fdd196030449e5a5d1d657a5c922377
ArulSelvan00 <arulselvanofficial5@gmail.com>
2025-06-13 21:34:15 +0530

    EmailService.java

--- src/main/java/com/SpringCrud/SpringAngular/service/EmailService.java
+++ src/main/java/com/SpringCrud/SpringAngular/service/EmailService.java
@@ -1,25 +1,22 @@
 package com.SpringCrud.SpringAngular.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+
     @Autowired
     private JavaMailSender mailSender;
 
-    @Autowired
-    private Environment env;
-
-    public void sendOrderConfirmationEmail(String toEmail, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(env.getProperty("spring.mail.username")); // sender
-        message.setTo(toEmail);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+    public void sendInvoice(String toEmail, String paymentId, String customerName) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(toEmail);
+        msg.setSubject("Invoice for Razorpay Payment");
+        msg.setText("Hello " + customerName + ",\n\nThank you for your payment.\nPayment ID: " + paymentId + "\n\nRegards,\nSree Sivam Sarees");
+        mailSender.send(msg);
     }
 }
